a780bb87990e0c5f73e6f38bd0bd8280cfc6b558
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-04 23:07:59 +0300

    Task 6 added

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿int value = new Random().Next(10, 100);
+System.Console.WriteLine((value));
+int result1 =  value % 10;
+int result2 = value / 10;
+if (result1 < result2)
+{
+    System.Console.WriteLine($"Первоe число {result2} больше второго числа {result1}");
+}
+else
+{
+    System.Console.WriteLine($"Второе число {result1} больше первого числа {result2}");
+}
