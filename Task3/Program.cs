5040b2ced2d543bc92b45e90726989eaeb4b9a0e
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-04 19:58:08 +0300

    Task3 added

---
+++ Task3/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int day = Convert.ToInt32(Console.ReadLine( ));
+string[] days = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+else
+{
+    Console.WriteLine(days [day -1]);
+}
+
+if (day == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (day == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (day == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (day == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (day == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (day == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (day == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+switch(day)
+{
+    case 1: Console.WriteLine("Понедельник");
+    break;
+    case 2: Console.WriteLine("Вторник");
+    break;
+    case 3: Console.WriteLine("Среда");
+    break;
+    case 4: Console.WriteLine("Четверг");
+    break;
+    case 5: Console.WriteLine("Пятница");
+    break;
+    case 6: Console.WriteLine("Суббота");
+    break;
+    case 7: Console.WriteLine("Воскресенье");
+    break;
+}
