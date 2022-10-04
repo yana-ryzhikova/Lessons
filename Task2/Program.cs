49d6a126e6ec261286b8da9d34a371860ad5b618
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-04 19:16:53 +0300

    Task2 added

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine( ));
+
+if (a == b*b)
+{
+    Console.WriteLine($"Число {a} является квадратом {b}.");
+}
+else if (b == a*a)
+{
+    Console.WriteLine($"Число {b} является квадратом {a}.");
+}
+else
+{
+    Console.WriteLine($"Числа не являются квадратами друг друга.");
+}
