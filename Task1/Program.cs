3799d51ed03bef7d83f4bf4f85aa8b27f10f374b
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-04 19:03:26 +0300

    Task1 added

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// напишите программу, которая на вход принимает число
+//и выдает его квадрат (т.е. умножает на само себя).
+// например:  4->16? -7->49
+
+
+int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string inputString = Console.ReadLine();
+    int value = Convert.ToInt32 (inputString);
+    return value;
+}
+
+int value = Prompt("Введите число: ");
+int square = value*value;
+Console.WriteLine($"Квадрат числа {value} = {square}");
+
+
