3aff08ec505872aee7835baf727da1d77a50a36a
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-04 23:07:33 +0300

    Task 4 added

---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу вычисления функции:
+// n = f(a)
+//
+// Напишите программу, которая на вход принимает число N и выводит все целые числа от -n до n.
+// -2 -1 0 1 2 
+
+System.Console.WriteLine("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -n;
+while (count <= n)
+{
+    System.Console.WriteLine(count);
+    count++;
+}
