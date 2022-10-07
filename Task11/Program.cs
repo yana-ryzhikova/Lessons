be117a1daf66445499f97fd044ae615f51558d14
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-07 22:07:44 +0300

    Added Task 11

---
+++ Task11/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая по заданному номеру четверти, 
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+
+
+int Prompt(string message)
+{
+    System.Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+bool ValidateCoords (int Quarter)
+{
+    if (Quarter > 4 && Quarter < 1)
+    {
+        System.Console.WriteLine("Вы ввели неправильную четверть");
+        return false;
+    }
+    return true;
+}
+
+string GetCoords (int Quarter)
+{
+    switch (Quarter)
+    {
+        case 1:
+        return "x > 0 && y > 0";
+        case 2:
+        return "x < 0 && y > 0";
+        case 3:
+        return "x < 0 && y < 0";
+        default:
+        return "x > 0 && y < 0";
+    }
+}
+int Quarter = Prompt ("Введите номер четверти ");
+if (!ValidateCoords(Quarter))
+{
+    return;
+}
+System.Console.WriteLine(GetCoords(Quarter));
