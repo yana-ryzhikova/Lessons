738e87c5cad9ab46eef350b37feb16bfdb3101b5
Yana Ryzhikova <ryzhikovayana@gmail.com>
2022-10-07 22:08:14 +0300

    Added Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Напишите программу, которая принимает на вход число (N) и 
+выдаёт таблицу квадратов чисел от 1 до N.
+5 -> 1, 4, 9, 16, 25.
+2 -> 1,4 */
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int N  = Prompt ("Введите число ");
+for (int i = 1; i <=N; i++)
+{
+    System.Console.WriteLine(Math.Pow(i,2));
+}
